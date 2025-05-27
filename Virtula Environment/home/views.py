ada161a05b79eff0c0cd8e7a36beb8e9f1f13ef5
Hackxyl <hackxyl58@gmail.com>
2025-05-27 12:44:59 +0300

    Add files via upload

---
+++ Virtula Environment/home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+   
+   
+    return render(request, 'home/base.html')
+
+
+def about(request):
+
+
+    return render(request, 'home/about.html')
+
+def project(request):
+
+
+    return render(request, 'home/project.html')
+
+def contact(request):
+
+    
+    return render(request, 'home/contact.html')
